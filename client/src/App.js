897f810457af83ea5f684348b044cba7ce387fdf
Chelly Compendio <rochellecompendio2021@u.northwestern.edu>
2019-05-12 15:01:21 -0500

    make home page
    added logo, intro, navbar with search

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import logo from './quarry.png';
+import icon from './search.png';
 import './App.css';
 import styles from './index.css';
 
@@ -20,13 +21,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+
+        {/* -----------------NAVIGATION BAR ------------------------*/}
+        <div className="nav">
+          {/* logo */}
+          <div><img className="quarry-logo" src={logo} alt="Logo" /></div>
+          {/* navbar search */}
+          <div className="whole-search" id="nav-search">
+            <div className="search-div"><input className="search-bar" type="text" value={this.state.value} placeholder="Search for a data set"></input></div>
+            <div><input className="search-icon" type="image" src={icon}/></div>
+          </div>
+        </div>
+
+        <div className="bg">
           <form onSubmit={this.handleSubmit}>
-            <h1>Type in a question!</h1>
-            <div className="search-bar">
-              <input type="text" value={this.state.value}></input>
+            <h1>Making Data Access Easy</h1>
+            <p className="intro">Quarry is a conversational system that provides users with a non-technical way to query large data sets. Simply type in questions about data in a data set, and Quarry will quickly supply and visualize answers!</p>
+            <p className="intro">Search for a data set to get started!</p>
+            <div className="whole-search">
+              <input className="search-bar" type="text" value={this.state.value} placeholder="Search for a data set"></input>
+              <div><input className="search-icon" type="image" src={icon}/></div>
             </div>
-            <input type="submit" value="Submit" />
+            {/* <div><input className="sub-button" type="submit" value="search" /></div> */}
           </form>
+          </div>
         </header>
       </div>
     );
